a4356183deaed753f19949dbfd73fe29e09428a1
syb <youremail@example.com>
2019-08-20 20:58:22 +0800

    新建登录和主页

--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@ import App from './App.vue'
 import router from './router'
 import ElementuI from 'element-ui' // 引入element组件
 import 'element-ui/lib/theme-chalk/index.css'// 引入样式
-import './style/index.less' // 引文件重置基础样式
+import './styles/index.less' // 引文件重置基础样式
 Vue.config.productionTip = false
 Vue.use(ElementuI) // 注册element组件
 new Vue({
--- src/router.js
+++ src/router.js
@@ -1,10 +1,16 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Home from './views/Home.vue'
+import Home from './views/home'
+import Login from './views/login/index.vue'
 Vue.use(Router)
 
 export default new Router({
   routes: [
+    {
+      path: '/login',
+      name: 'login',
+      component: Login
+    },
     {
 
       path: '/',
