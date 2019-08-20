e45fac467e5641f3334efe744f772a718655329b
syb <youremail@example.com>
2019-08-20 19:13:56 +0800

    安装Elem并使用

--- src/main.js
+++ src/main.js
@@ -1,9 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
-import ElementuI from 'element-ui' // 引入UI
+import ElementuI from 'element-ui' // 引入element组件
+import 'element-ui/lib/theme-chalk/index.css'// 引入样式
 Vue.config.productionTip = false
-Vue.use(ElementuI)
+Vue.use(ElementuI) // 注册element组件
 new Vue({
   router,
   render: h => h(App)
