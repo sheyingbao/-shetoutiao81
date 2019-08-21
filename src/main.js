8644a5d2297137ab845222f9bcbb5dd6ee3c128a
syb <youremail@example.com>
2019-08-21 11:17:40 +0800

    配置全局使用

--- src/main.js
+++ src/main.js
@@ -4,6 +4,11 @@ import router from './router'
 import ElementuI from 'element-ui' // 引入element组件
 import 'element-ui/lib/theme-chalk/index.css'// 引入样式
 import './styles/index.less' // 引文件重置基础样式
+import axios from 'axios' //  引入axios
+// 将后台接口的地址给axios的bashURL
+axios.defaults.baseURL = 'http:ttapi.research.itcast.cn/mp/v1_0'
+Vue.prototype.$axios = axios // 把axios插件给了Vue对象的原型属性
+
 Vue.config.productionTip = false
 Vue.use(ElementuI) // 注册element组件
 new Vue({
--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -5,21 +5,24 @@
       <img src="../../assets/img/logo_index.png" alt="黑马头条" />
       </div>
       <!-- 表单结构 el-form el-form-item -->
-      <el-form>
-      <el-form-item>
-          <!-- 手机号绑定 -->
+      <el-form v-bind:model="formData"  :rules="rules" ref="loginForm">
+        <!-- el-form-item 的prop的属性设置为需要效验的字段 -->
+      <el-form-item prop='mobile'>
+          <!-- 手机号绑定 input-->
         <el-form-item >
           <el-input  v-model="formData.mobile" placeholder="请输入手机号"></el-input>
         </el-form-item>
-        <el-form-item prop="code">
-          <el-input  placeholder="请输入验证码" style="width:240px"></el-input>
-          <el-button style="float:right">发送验证码</el-button>
+        <el-form-item prop='code'>
+          <!-- 验证码绑定input -->
+          <el-input  placeholder='请输入验证码' style='width:240px'></el-input>
+          <el-button  v-model="formData.code" style='float:right'>发送验证码</el-button>
         </el-form-item>
-        <el-form-item>
+        <el-form-item prop="check">
+          <!-- checkbox绑定check -->
           <el-checkbox :checked="true">我已阅读并同意用户协议和隐私条款</el-checkbox>
         </el-form-item>
         <el-form-item>
-          <el-button  type="primary" style="width:100%">登录</el-button>
+          <el-button @click="login" type="primary" style="width:100%">登录</el-button>
         </el-form-item>
       </el-form>
     </el-card>
@@ -29,16 +32,56 @@
 <script>
 export default {
   data () {
+    // 定义一个自定义函数
+    var func = function (rule, value, callback) {
+      //  rule 是当前的效验规则  没什么用
+      // value 就是当前表单的字段的值
+      if (value) {
+        // 满足效验
+        callback() // 同意继续往下走
+      } else {
+        // 否则不满足效验
+        callback(new Error('您必须无条件同意被坑'))
+      }
+    }
     return {
       // 定义表单数据对象
       formData: {
         mobile: '',
-        cold: '',
+        code: '',
         check: false
+      },
+      rules: {
+        mobile: [
+          { required: true, message: '登录手机号不能为空', trigger: 'blur' },
+          { pattern: /^1[3456789]\d{9}$/, message: '手机号格式错误' }
+        ],
+        code: [
+          { required: true, message: '验证码不能为空', trigger: 'blur' },
+          { pattern: /^\d{6}$/, message: '验证码为6位字符' }
+        ],
+        check: [{
+          //  message: '您必须无条件同意被坑',
+          validator: func // 自定义函数
+        }]
       }
+    }
+  },
+  methods: {
+    login () {
+      // validate  手动效验表单数据
+      // isOK是 当前表单是否通过了校验
+      // result 是当前的校验结果
+      this.$refs.loginForms.validate(isOK => {
+        if (isOK) {
+          //  通过了效验
+          console.log('前端校验成功')
+        }
+      })
     }
   }
 }
+
 </script>
 
 <style lang="less" scoped>
