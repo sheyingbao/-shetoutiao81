db1fa00f8087977df1512a741595c80bb4e4878e
syb <youremail@example.com>
2019-08-22 11:00:14 +0800

    登录成功及失败处理

--- src/router.js
+++ src/router.js
@@ -13,7 +13,7 @@ export default new Router({
     },
     {
       path: '/',
-      redirect: 'home'
+      redirect: '/home'
     },
     {
 
--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -5,21 +5,21 @@
       <img src="../../assets/img/logo_index.png" alt="黑马头条" />
       </div>
       <!-- 表单结构 el-form el-form-item -->
-      <el-form v-bind:model="formData"  :rules="rules" ref="loginForm">
+      <el-form :model="formData"  :rules="rules" ref="loginForm">
         <!-- el-form-item 的prop的属性设置为需要效验的字段 -->
       <el-form-item prop='mobile'>
           <!-- 手机号绑定 input-->
-        <el-form-item >
+
           <el-input  v-model="formData.mobile" placeholder="请输入手机号"></el-input>
         </el-form-item>
         <el-form-item prop='code'>
           <!-- 验证码绑定input -->
-          <el-input  placeholder='请输入验证码' style='width:240px'></el-input>
-          <el-button  v-model="formData.code" style='float:right'>发送验证码</el-button>
+          <el-input  placeholder='请输入验证码' v-model="formData.code" style='width:240px'></el-input>
+          <el-button   style='float:right'>发送验证码</el-button>
         </el-form-item>
         <el-form-item prop="check">
           <!-- checkbox绑定check -->
-          <el-checkbox :checked="true">我已阅读并同意用户协议和隐私条款</el-checkbox>
+          <el-checkbox :checked="false">我已阅读并同意用户协议和隐私条款</el-checkbox>
         </el-form-item>
         <el-form-item>
           <el-button @click="login" type="primary" style="width:100%">登录</el-button>
@@ -61,7 +61,6 @@ export default {
           { pattern: /^\d{6}$/, message: '验证码为6位字符' }
         ],
         check: [{
-          //  message: '您必须无条件同意被坑',
           validator: func // 自定义函数
         }]
       }
@@ -72,7 +71,7 @@ export default {
       // validate  手动效验表单数据
       // isOK是 当前表单是否通过了校验
       // result 是当前的校验结果
-      this.$refs.loginForms.validate(isOK => {
+      this.$refs.loginForm.validate(isOK => {
         if (isOK) {
           //  通过了效验
           this.$axios({
@@ -83,6 +82,12 @@ export default {
             window.localStorage.setItem('user-info', JSON.stringify(result.data.data))
             //  编程式导航
             this.$router.push('/home')
+          }).catch(() => {
+            // ￥message是elementui注入到实例的方法
+            this.$message({
+              message: '当前用户名或则密码错误',
+              type: 'warning'
+            })
           })
         }
       })
