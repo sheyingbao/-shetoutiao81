d16dc246e4ad3af3fb7abf456aa509ae3497369a
syb <youremail@example.com>
2019-08-22 11:19:40 +0800

    主页布局

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,5 +1,18 @@
 <template>
-  <div>home</div>
+<!-- 最外层容器 -->
+<el-container>
+<!-- 左侧边栏容器 -->
+<el-aside>
+  左侧内容
+</el-aside>
+<!-- 嵌套大内容 -->
+<el-container>
+<!-- 头部内容 -->
+<el-header>头部内容</el-header>
+<!-- 中间区域 -->
+<el-main>中间区域</el-main>
+</el-container>
+</el-container>
 </template>
 
 <script>
