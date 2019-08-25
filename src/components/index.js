fb4d5704beab294d066472e8fc60a2de4a28d64c
syb <youremail@example.com>
2019-08-25 22:02:49 +0800

    面包屑分装组件

--- src/components/index.js
+++ src/components/index.js
@@ -1,8 +1,10 @@
 import layoutHeader from './home/layout-header.vue'
 import layoutAside from './home/layout-aside.vue'
+import breadCrumb from './common/bread-crumb'
 export default {
   install (Vue) {
     Vue.component('layout-header', layoutHeader)
     Vue.component('layout-aside', layoutAside)
+    Vue.component('bread-crumb', breadCrumb)// 注册面包屑组件
   }
 }
