b6cf01130584096e0c39ef7a6443dda5bbf6e63c
syb <youremail@example.com>
2019-08-26 20:26:54 +0800

    评论列表-页面结构

--- src/views/comment/index.vue
+++ src/views/comment/index.vue
@@ -1,10 +1,28 @@
 <template>
-<div>评论列表</div>
+<el-card>
+     <!-- <bread-crumb>面包屑  具名插槽 header -->
+    <bread-crumb slot='header'>
+    <!-- 面包屑的插槽  具名插槽 title -->
+    <template solt='title'> 评论列表 <template>
+     </bread-crumb>
+     <!-- 表格组件   label 表格标题-->
+     <el-table >
+      <el-table-column label='标题'></el-table-column>
+      <el-table-column label='评论状态'></el-table-column>
+      <el-table-column label='总评论数'></el-table-column>
+      <el-table-column label='粉丝评论数'></el-table-column>
+      <el-table-column label='操作'></el-table-column>
+     </el-table>
+</el-card>
 </template>
 
 <script>
 export default {
-
+  data () {
+    return {
+      list: []
+    }
+  }
 }
 </script>
 
